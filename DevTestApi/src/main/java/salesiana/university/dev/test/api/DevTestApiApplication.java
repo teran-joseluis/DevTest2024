ec7cbdbf22fe67f15f298b9b43e8a765647b5c05
Terán Rocha José Luis <joseluisteran2617@gmail.com>
2024-11-09 09:51:15 -0400

    chore: set up Java project using `Maven`

---
+++ DevTestApi/src/main/java/salesiana/university/dev/test/api/DevTestApiApplication.java
@@ -0,0 +1,13 @@
+package salesiana.university.dev.test.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevTestApiApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DevTestApiApplication.class, args);
+  }
+
+}
---
+++ DevTestApi/src/test/java/salesiana/university/dev/test/api/DevTestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package salesiana.university.dev.test.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevTestApiApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
